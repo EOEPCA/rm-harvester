934a280f08ac198c3d9c544a68137d70d5bc6e92
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2022-09-09 10:42:19 +0200

    Add a thumbnail item if it exists

--- harvester_eoepca/postprocess.py
+++ harvester_eoepca/postprocess.py
@@ -1,9 +1,12 @@
 import json
 import logging
+from os.path import basename, splitext, normpath, join
 from urllib.parse import urlparse
 
 import boto3
+import botocore
 from harvester.abc import Postprocessor
+import pystac
 from pystac.stac_io import DefaultStacIO, StacIO
 from stactools.sentinel2.stac import create_item
 from stactools.sentinel2.product_metadata import ProductMetadata
@@ -38,6 +41,19 @@ class CREODIASS3StacIO(DefaultStacIO):
         else:
             return super().read_text_from_href(href)
 
+    def exists(self, path):
+        """ Checks whether the given object exists on the storage
+        """
+        parsed = urlparse(path)
+        try:
+            CREODIASS3StacIO().s3.Object(parsed.netloc, parsed.path[1:]).load()
+            return True
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return False
+            else:
+                raise
+
 
 class CREODIASOpenSearchSentinel2Postprocessor(Postprocessor):
     """ Takes the result of a Sentinel-2 OpenSearch search and enriches the
@@ -48,12 +64,26 @@ class CREODIASOpenSearchSentinel2Postprocessor(Postprocessor):
         StacIO.set_default(CREODIASS3StacIO)
         path = item['properties']['productIdentifier']
         path = path.replace('/eodata/', 's3://EODATA/') + '/'
-        stac_item = create_item(path).to_dict(include_self_link=False)
+        stac_item: pystac.Item = create_item(path)
+
+        # see if we the thumbnail exists, if yes, add it to the STAC Item
+        ql_path = join(path, f"{splitext(basename(normpath(path)))[0]}-ql.jpg")
+        if CREODIASS3StacIO().exists(ql_path):
+            stac_item.add_asset(
+                "thumbnail",
+                pystac.Asset(
+                    ql_path,
+                    media_type="image/jpeg",
+                    roles=["thumbnail"],
+                ),
+            )
+
+        out_item = stac_item.to_dict(include_self_link=False)
         if LOGGER.isEnabledFor(logging.DEBUG):
             LOGGER.debug(json.dumps(stac_item, indent=4))
 
         # reset the
-        stac_item["id"] = ProductMetadata(
-            stac_item["assets"][PRODUCT_METADATA_ASSET_KEY]["href"]
+        out_item["id"] = ProductMetadata(
+            out_item["assets"][PRODUCT_METADATA_ASSET_KEY]["href"]
         ).product_id
-        return stac_item
+        return out_item
